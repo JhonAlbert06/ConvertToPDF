48fbd5374ee648d102b1bf41b92fd4c95671c6a7
Jhon Albert Guzman Rosario <79234293+JhonAlbert06@users.noreply.github.com>
2025-04-15 13:52:10 -0400

    Changes

--- ConvertToPDF/obj/Debug/net8.0/ConvertToPDF.AssemblyInfo.cs
+++ ConvertToPDF/obj/Debug/net8.0/ConvertToPDF.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("ConvertToPDF")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+745e02b8aa9a11979a9ddc64aa929d63c8a8efc1")]
 [assembly: System.Reflection.AssemblyProductAttribute("ConvertToPDF")]
 [assembly: System.Reflection.AssemblyTitleAttribute("ConvertToPDF")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
